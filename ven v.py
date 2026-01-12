b171537a8faca9ba691168da69b288a2ee4e21d5
andrewseusebio <eusebiofcp@outlook.com>
2026-01-11 23:16:23 -0300

    Add files via upload

---
+++ ven v.py
@@ -0,0 +1,4 @@
+TOKEN=8227819693:AAGm7y4oN4CBotK2qQiRapegjIcIYmlIBLc
+DATABASE_URL=postgresql://postgres:fEgruEVZPbKAAsYTsDspdtIhptuKpysS@postgres.railway.internal:5432/railway
+ADMINS=8276989322
+GRUPO_TELEGRAM=-1003534782614
