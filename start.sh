3965a0f5f19779be813cfc9c1ea539135bd2e195
andrewseusebio <eusebiofcp@outlook.com>
2026-01-11 22:59:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-python3 bot.py
 
+# Verifique se o Python 3 está instalado e crie um ambiente virtual
+python3 -m venv venv
+
+# Ative o ambiente virtual
+source venv/bin/activate
+
+# Instale as dependências do projeto
+pip install -r requirements.txt
+
+# Inicie o bot
+python3 bot.py
