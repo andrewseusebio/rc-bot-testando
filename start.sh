ad0381b28fbfee337434e712f335c10c943fc5c3
andrewseusebio <eusebiofcp@outlook.com>
2026-01-11 23:11:45 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Se você estiver usando um ambiente virtual, descomente a linha abaixo:
+# source venv/bin/activate
+
+# Instalar as dependências
+pip install -r requirements.txt
+
+# Iniciar o bot
+python3 bot.py
